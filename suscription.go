397192893100fcf49bd0ffbd2441a9ea660dc601
vicmanbrile <victor.briseno.leon@hotmail.com>
2021-12-23 17:46:50 -0600

    documentation and code for suscriptions

--- suscription.go
+++ suscription.go
@@ -1,5 +1,18 @@
 package main
 
 type Suscription struct {
-	Name string
+	Name    string  `json:"name"`
+	Type    string  `json:"type"`
+	Pricing float64 `json:"pricing"`
+}
+
+func (s Suscription) PriceMount() float64 {
+	switch s.Type {
+	case "monthly":
+		return s.Pricing
+	case "yearly":
+		return s.Pricing / float64(12)
+	default:
+		return 0
+	}
 }
