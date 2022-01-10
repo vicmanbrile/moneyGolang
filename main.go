28cff3607d060f50d85ca821048ce86c7532f25b
vicmanbrile <victor.briseno.leon@hotmail.com>
2022-01-10 09:51:00 -0600

    make dir module profile

--- main.go
+++ main.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"fmt"
 	"io/ioutil"
+
+	"github.com/vicmanbrile/moneyGolang/profile"
 )
 
 func main() {
@@ -13,7 +15,7 @@ func main() {
 		fmt.Printf("Error al convertir a JSON: %v", err)
 	}
 
-	data := &Perfil{}
+	data := &profile.Perfil{}
 
 	err = json.Unmarshal([]byte(file), &data)
 	if err != nil {
--- profile/wallet.go
+++ profile/wallet.go
@@ -1,4 +1,4 @@
-package main
+package profile
 
 type Wallet struct {
 	Cash    float64 `json:"cash"`
