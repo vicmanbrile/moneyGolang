acc2929e51ccfae06a965da394f35dff472a894f
vicmanbrile <victor.briseno.leon@hotmail.com.mx>
2022-01-13 23:30:06 -0600

    modify gocarina_gocsv

--- go.mod
+++ go.mod
@@ -6,3 +6,5 @@ require (
 	github.com/mattn/go-runewidth v0.0.9 // indirect
 	github.com/olekukonko/tablewriter v0.0.5
 )
+
+require github.com/gocarina/gocsv v0.0.0-20211203214250-4735fba0c1d9 // indirect
--- status/status.go
+++ status/status.go
@@ -1,38 +1,48 @@
 package status
 
 import (
-	"bufio"
-	"encoding/csv"
 	"fmt"
-	"io"
-	"log"
 	"os"
-	"strconv"
-	"time"
+
+	"github.com/gocarina/gocsv"
 )
 
+type Status struct { // Our example struct, you can use "-" to ignore a field
+	Id    string  `csv:"Clave"`
+	Value float64 `csv:"Valor"`
+}
+
 var (
-	Year               int     = 2022
-	porcentajeAGuardar float64 = 73.82
-	dineroPromedio     float64 = 240.00
+// Year int = 2022
+// porcentajeAGuardar float64 = 73.82
+// dineroPromedio     float64 = 240.00
 )
 
 func Resumen(sumas ...string) {
-	var tenemos [][]float64
 
-	tenemos = append(tenemos, ReadFiles(sumas[0])) // Gastados
-	tenemos = append(tenemos, ReadFiles(sumas[1])) // Guardados
-	tenemos = append(tenemos, ReadFiles(sumas[2])) // Extra
+	var all = make([][]*Status, 3)
 
-	fmt.Printf("Dia %.2f\n", automaticTime(Year))
-	fmt.Printf("Presupuesto: %.2f \n", presupuesto())
-	Falta := presupuesto() - sumaTenemos(tenemos)
-	fmt.Printf("Falta: %.2f\n", Falta)
-	fmt.Printf("Status %.2f\n", (0-Falta)/(porcentajeAGuardar*dineroPromedio))
-	fmt.Println("Libres", ((dineroPromedio * ((100 - porcentajeAGuardar) / 100)) * automaticTime(Year)))
+	for index, value := range sumas {
+		result := ReadFiles(value)
+		all[index] = result
+	}
+
+	for _, value := range all {
+		for _, value := range value {
+			fmt.Println(*value)
+		}
+	}
+
+	// fmt.Printf("Dia %.2f\n", automaticTime(Year))
+	// fmt.Printf("Presupuesto: %.2f \n", presupuesto())
+	// Falta := presupuesto() - sumaTenemos(tenemos)
+	// fmt.Printf("Falta: %.2f\n", Falta)
+	// fmt.Printf("Status %.2f\n", (0-Falta)/(porcentajeAGuardar*dineroPromedio))
+	// fmt.Println("Libres", ((dineroPromedio * ((100 - porcentajeAGuardar) / 100)) * automaticTime(Year)))
 
 }
 
+/*
 func automaticTime(year int) float64 {
 	inicio := time.Date(year, time.January, 0, 0, 0, 0, 0, time.UTC)
 	calculo := time.Now().AddDate(0, 0, -inicio.Day()).Day()
@@ -58,24 +68,13 @@ func sumaTenemos(total [][]float64) float64 {
 
 	return result
 }
+*/
 
-func ReadFiles(arg string) (total []float64) {
-	file, _ := os.Open(arg)
-	r := csv.NewReader(bufio.NewReader(file))
-	r.Comma = ','
+func ReadFiles(arg string) (total []*Status) {
+	file, _ := os.OpenFile(arg, os.O_RDWR|os.O_CREATE, os.ModePerm)
 
-	for {
-		record, err := r.Read()
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		num, _ := strconv.Atoi(record[1])
-
-		total = append(total, float64(num))
+	if err := gocsv.UnmarshalFile(file, &total); err != nil { // Load clients from file
+		panic(err)
 	}
 
 	return
