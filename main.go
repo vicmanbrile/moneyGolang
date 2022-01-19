00efa5c2623e2286fa6257b2e25bc7aaef25996f
vicmanbrile <victor.briseno.leon@hotmail.com>
2022-01-19 17:35:26 -0600

    calc to time dave and now

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"fmt"
+
 	database_mongodb "github.com/vicmanbrile/moneyGolang/database"
 )
 
@@ -10,5 +12,20 @@ func main() {
 	app.Init()
 
 	app.Perfil.PrintTable()
+	A := app.Perfil.Registers.BudgetsNow(app.Perfil.Wallets.Average)
+	B := app.Perfil.Registers.BudgetsWon(app.Perfil.Wallets.Average)
+	{
+		fmt.Printf("Dinero para ahora:%.2f , Dinero de entregado: %.2f\n", A.Total, B.Total)
+	}
+	{
+		C := A.Lack((app.Perfil.PriceDays() / app.Perfil.Wallets.Average))
+		D := B.Lack((app.Perfil.PriceDays() / app.Perfil.Wallets.Average))
+		fmt.Printf("Dinero para ahora tenemos:%.2f , Dinero de entregado tenemos: %.2f\n", C, D)
+	}
+	{
+		C := A.Free(1 - (app.Perfil.PriceDays() / app.Perfil.Wallets.Average))
+		D := B.Free(1 - (app.Perfil.PriceDays() / app.Perfil.Wallets.Average))
+		fmt.Printf("Dinero para ahora Libres:%.2f , Dinero de entregado Libres: %.2f\n", C, D)
+	}
 
 }
--- status/status.go
+++ status/status.go
@@ -1,10 +1,7 @@
 package status
 
 import (
-	"fmt"
 	"time"
-
-	"github.com/vicmanbrile/moneyGolang/profile"
 )
 
 type Registers struct {
@@ -23,59 +20,36 @@ type Registers struct {
 	} `json:"extras"`
 }
 
-// Presupuesto por ganados | Falta | Libres | Estatus | Libres
-// Presupuesto por ahora   | Falta | Libres | Estatus | Libres
-
-func (r *Registers) BudgetsNow(w profile.Wallet) float64 {
-	days := automaticTime(Year)
-	return days * w.Average
+type Budget struct {
+	Total float64
 }
 
-func (r *Registers) BudgetsWon(w profile.Wallet) {}
-
-var (
-	Year               int     = 2022
-	porcentajeAGuardar float64 = 73.82
-	dineroPromedio     float64 = 240.00
-)
-
-func Resumen(sumas ...string) {
-	/*
-		var all = make([][]*Valores, 3)
-
-		for index, value := range sumas {
-			result := ReadFiles(value)
-			all[index] = result
-		}
-
-		var tenemos float64
-		for _, v := range all[0] {
-			tenemos += v.Value
-		}
-		for _, v := range all[1] {
-			tenemos += v.Value
-		}
-		for _, v := range all[2] {
-			tenemos -= v.Value
-		}
-	*/
-
-	fmt.Printf("Dia %.2f\n", automaticTime(Year))
-	fmt.Printf("Presupuesto: %.2f \n", presupuesto())
-	Falta := presupuesto() // - tenemos
-	fmt.Printf("Falta: %.2f\n", Falta)
-	fmt.Printf("Status %.2f\n", (0-Falta)/((porcentajeAGuardar/100)*dineroPromedio))
-	fmt.Printf("Libres: %.2f\n", ((dineroPromedio * ((100 - porcentajeAGuardar) / 100)) * automaticTime(Year)))
-
+func (r *Registers) BudgetsNow(m float64) (Bdgt Budget) {
+	days := automaticTime()
+	Bdgt.Total = days * m
+	return
 }
 
-func automaticTime(year int) float64 {
-	inicio := time.Date(year, time.January, 0, 0, 0, 0, 0, time.UTC)
-	calculo := time.Now().AddDate(0, 0, -inicio.Day()).Day()
+func (r *Registers) BudgetsWon(m float64) (Bdgt Budget) {
+	var days float64
+	for _, value := range r.Extras {
+		days += value.Days
+	}
 
-	return float64(calculo)
+	Bdgt.Total = days * m
+	return
 }
 
-func presupuesto() float64 {
-	return dineroPromedio * (porcentajeAGuardar / 100) * float64(automaticTime(Year))
+func (b *Budget) Lack(percentage float64) float64 {
+	return b.Total * percentage
+}
+
+func (b *Budget) Free(percentage float64) float64 {
+	return b.Total * percentage
+}
+
+func automaticTime() float64 {
+	today := time.Now().YearDay()
+
+	return float64(today)
 }
